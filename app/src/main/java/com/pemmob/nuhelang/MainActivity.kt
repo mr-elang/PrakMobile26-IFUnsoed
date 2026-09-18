88f6bec989d5bfff6b2cc0f2f12e82ddc715cae2
mr-elang <nuhelang@gmail.com>
2026-09-19 01:47:36 +0700

    update pertemuan 2

--- app/src/main/java/com/pemmob/nuhelang/MainActivity.kt
+++ app/src/main/java/com/pemmob/nuhelang/MainActivity.kt
@@ -15,7 +15,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,6 +28,11 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.pemmob.nuhelang.ui.screen.BasicInfoScreen
+import com.pemmob.nuhelang.ui.screen.HubungiKamiScreen
 import com.pemmob.nuhelang.ui.theme.JualanTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,8 +41,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JualanTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LayoutTentangJualan(modifier = Modifier.padding(innerPadding))
+                Surface(
+                    modifier = Modifier.fillMaxWidth(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "basic_info"){
+                        composable(route = "basic_info") {
+                            BasicInfoScreen(
+                                onNavigateToContact = {navController.navigate(route = "form_screen")}
+                            )
+                        }
+                        composable(route = "form_screen"){
+                            HubungiKamiScreen(navController = navController)
+                        }
+                    }
                 }
             }
         }
